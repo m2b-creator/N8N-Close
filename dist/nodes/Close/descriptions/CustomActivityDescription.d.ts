f49640c55ed08639ce3e7db9aa2da1fb74c4818e
m2b-creator <maurimubo@gmail.com>
2025-09-19 09:39:17 +0100

    Restores the code to version 1.0.27 and increments version to 1.0.31

--- dist/nodes/Close/descriptions/CustomActivityDescription.d.ts
+++
@@ -1,3 +0,0 @@
-import type { INodeProperties } from 'n8n-workflow';
-export declare const customActivityOperations: INodeProperties[];
-export declare const customActivityFields: INodeProperties[];
